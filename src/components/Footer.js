39e8094b37533df8449abf7799706c1d87f3711d
neverstop2077 <quocduy060220@gmail.com>
2022-04-19 11:38:41 +0700

    edit sth

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = props => {
     return (
         <div className='footer'>
-            <p className='footer-words'>Design by me. @Vo Quoc Duy 04-2020</p>
+            <p className='footer-words'>Design by me. @Vo Quoc Duy 04-2022</p>
         </div>
     );
 };
